ee1a431b99b558bf81bb6db99c4bd8475002df7e
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-25 09:47:37 +1200

    Feat: Init Homepage

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss";
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className='home'>
+            HomePage 
+        </div>
+    );
+}
+
+export default Home;
