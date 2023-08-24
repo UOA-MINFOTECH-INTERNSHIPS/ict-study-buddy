98d19f9bfd8844554b13fd5eaf9b214f63be902c
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-25 09:54:09 +1200

    Feat: Init Profile Page

---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./profile.scss";
+
+function Profile(props) {
+    return (
+        <div className='profile'>
+            
+        </div>
+    );
+}
+
+export default Profile;
