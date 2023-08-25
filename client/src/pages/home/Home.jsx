a2bd38cc1201e9fa899b571a229127557659d84f
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-25 14:44:24 +1200

    Feat: Init HomePage Layout

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import "./home.scss";
-import React from 'react';
+import * as React from "react";
+import Stories from "../../components/stories/Stories";
+import Posts from "../../components/posts/Posts";
+import Shares from "../../components/shares/Shares";
 
-function Home(props) {
-    return (
-        <div className='home'>
-            HomePage 
-        </div>
-    );
+function home(props) {
+  return (
+    <div className="home">
+      <Stories />
+      <Shares />
+      <Posts />
+    </div>
+  );
 }
 
-export default Home;
+export default home;
