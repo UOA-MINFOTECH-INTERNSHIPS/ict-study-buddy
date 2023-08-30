681ce62c8026643ab4b4afa1d0ac7f4f9772acc4
CathyTanUOA <126939857+CathyTanUOA@users.noreply.github.com>
2023-08-31 11:03:24 +1200

    Replace Header with navbar components

--- client/src/pages/MessagingPage/Header.jsx
+++ client/src/pages/MessagingPage/Header.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  
-import './MessagingPage.css';
+import "./navbar.scss";
+import * as React from "react";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import photo from "../../assets/register-background-pic.jpg";
 
-function Header() {
-    return (
-        <div className="header">
-            <span>Study Buddy</span>
-            <div>
-                <a href="community.html">
-                    <img src="./community.png" alt="Community Icon" /> Community
-                </a>
-                <Link to="/">
-                    <img src="./message.png" alt="Message Icon" /> Messages
-                </Link>
-                <Link to="/settings">
-                    <img src="./setting.png" alt="Settings Icon" /> Settings
-                </Link>
-            </div>
+function NavBar(props) {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Study Buddy</span>
+        </Link>
+        <HomeOutlinedIcon />
+
+        <WbSunnyOutlinedIcon style={{ cursor: "pointer" }} />
+
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search" />
         </div>
-    );
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <MessageOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src={photo} />
+          <span>Jenny</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default NavBar;
