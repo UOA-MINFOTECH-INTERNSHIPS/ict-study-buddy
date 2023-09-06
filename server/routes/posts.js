a53e99ad8407fb0e02c9b33fe9a6926742b02723
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-06 13:33:38 +1200

    Feat: Create Delete Post API

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -14,4 +14,19 @@ router.post("/", async (req, res) => {
   }
 });
 
+// DELETE A POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("Post has been deleted.");
+    } else {
+      return res.status(403).json("You can only delete your own post.");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 export default router;
