9db5f43a33ccace546c6c77793d7e474483ccf08
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-06 13:34:28 +1200

    Feat: Create Update Post API

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -29,4 +29,20 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+
+// UPDATE A POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post has been updated.");
+    } else {
+      return res.status(403).json("You can only update your own post.");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 export default router;
