8c93d5651591883565bee2da569e0f1f4c076159
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-06 12:07:59 +1200

    Feat: Initialised Server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmt from "helmet";
+import morgan from "morgan";
+
+
+//Setup Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmt());
+app.use(morgan("common"));
+
+
+
+
+// Start the server running. Once the server is running, the given function will be called, which will
+// log a simple message to the server console. Any console.log() statements in your node.js code
+// can be seen in the terminal window used to run the server.
+// await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+app.listen(port, () => console.log(`App server listening on port ${port}!`));
