17d9415c736866e57caf0375b627c309e35b1f6d
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-12 17:19:49 +1200

    Update: Optimize AuthContext

--- server/server.js
+++ server/server.js
@@ -16,7 +16,12 @@ const port = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 app.use(helmt());
 app.use(morgan("common"));
 
