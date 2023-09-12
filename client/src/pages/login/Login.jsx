79ee26a5202e6a0d6807579e95293e77dcb61f47
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-12 17:33:54 +1200

    Update: Optimize Login Form with Validation and Error Feedback and Auto-redirect to login after successful registration

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,33 @@
 import "./login.scss";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
-import { makeRequest } from "../../axios";
 import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// Login Form Validation
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("Email must be valid")
+    .required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
 
 function Login(props) {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
 
   const onSubmit = async (data) => {
     const user = {
@@ -21,13 +35,19 @@ function Login(props) {
       password: data.password,
     };
     try {
-      await makeRequest.post("/auth/login", user);
+      await login(user);
       navigate("/");
     } catch (error) {
-      console.log("error", error);
+      setError(error.response.data);
     }
   };
 
+  // Function to clear the error state
+  const clearError = () => {
+    setError(null);
+  };
+
+  
   return (
     <div className="login">
       <div className="card">
@@ -45,12 +65,18 @@ function Login(props) {
         <div className="right">
           <h1>Login</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} type="text" placeholder="Email" />
+            <input {...register("email")} type="email" placeholder="Email" onChange={clearError}/>
+            {errors.email && <p className="error">{errors.email?.message}</p>}
             <input
               {...register("password")}
               type="password"
               placeholder="Password"
+              onChange={clearError}
             />
+            {errors.password && (
+              <p className="error">{errors.password?.message}</p>
+            )}
+            {error && <p className="error">{error}</p>}
             <input type="submit" className="submit" />
           </form>
         </div>
