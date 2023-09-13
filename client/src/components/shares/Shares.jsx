14232a2539c6a38e5f6e48159f6a5db3ce1a1c51
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-13 14:26:24 +1200

    Update: Update with active user data

--- client/src/components/shares/Shares.jsx
+++ client/src/components/shares/Shares.jsx
@@ -1,43 +1,48 @@
-import * as React from "react";
+import React from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 import "./share.scss";
 import { Link } from "react-router-dom";
 import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
-import VideocamIcon from "@mui/icons-material/Videocam";
+import TagIcon from '@mui/icons-material/Tag';
 
 function Shares(props) {
   const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="share">
       <div className="container">
-        <Link to={`/profile/${currentUser.id}`} style={{textDecoration: "none", color: "inherit"}}>
+        {/* UserInfo */}
+        <Link to={`/profile/${currentUser.userName}`} style={{textDecoration: "none", color: "inherit"}}>
           <div className="userInfo">
             <img src={currentUser.profilePic} alt="avatar" />
-            <span className="name">{currentUser.name}</span>
+            <span className="name">{currentUser.userName}</span>
           </div>
         </Link>
+        {/* Input Content  */}
         <div className="content">
           <input
             type="text"
-            placeholder={`What's on your mind, ${currentUser.name}?`}
+            placeholder={`What's on your mind, undefined?`}
           />
         </div>
         <hr />
+        {/* Add attachment  */}
         <div className="attachment">
           <div className="left">
             <div className="item">
               <input type="file" id="file" />
               <label htmlFor="file">
                 <InsertPhotoIcon />
-                <span htmlFor="file">Add photo</span>
+                <span htmlFor="file">Add File</span>
               </label>
             </div>
+            {/* Add Course Tags or Skills Tags */}
             <div className="item">
               <input type="file" id="file" />
               <label htmlFor="file">
-                <VideocamIcon />
-                <span htmlFor="file">Add video</span>
+                <TagIcon />
+                <span htmlFor="file">Add Tags</span>
               </label>
             </div>
           </div>
