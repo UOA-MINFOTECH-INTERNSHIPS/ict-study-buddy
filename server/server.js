f70f597c21c62650dc52b138f34f7a45136bd876
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-22 12:18:42 +1200

    Feat: Create File Upload storage and API

--- server/server.js
+++ server/server.js
@@ -2,33 +2,71 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
 import mongoose from "mongoose";
 import cors from "cors";
 import helmt from "helmet";
 import morgan from "morgan";
-import authRoute from "./routes/auth.js";
+import multer from "multer";
 import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
 import postRoute from "./routes/posts.js";
 import cookieParser from "cookie-parser";
-
 //Setup Express
 const app = express();
 const port = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 //Middleware
+// Setup JSON parsing for the request body
 app.use(express.json());
+
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
-app.use(helmt());
+  cors({
+    origin: ["https://study-buddy-steel.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(
+  helmt({
+    crossOriginResourcePolicy: false,
+  })
+);
 app.use(morgan("common"));
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
+//Set up sotrage destination and filename
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+//Set up upload file api
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    const file = req.file;
+    res.status(200).json(file.filename);
+  } catch (error) {
+    console.log("error", error);
+  }
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// Setup API routes.
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 
 // Start the server running. Once the server is running, the given function will be called, which will
