a3012dd82acb09e0194c6b38d708df9c17f6de91
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-29 15:01:10 +1300

    Update: Update Add a Comment API with new property

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -2,27 +2,35 @@ import * as React from "react";
 import "./comments.scss";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
 
 function Comments(props) {
   const { currentUser } = useContext(AuthContext);
-  const comments = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Monica",
-      userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/1313254/pexels-photo-1313254.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-    {
-      id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Jenny",
-      userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/16943679/pexels-photo-16943679/free-photo-of-ranti-marsyanda-chandri-anggara.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-  ];
+
+  const {isLoading, error, data: comments} = useQuery(["comments"], makeRequest.get()
+
+  );
+  
+
+  // const comments = [
+  //   {
+  //     id: 1,
+  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+  //     name: "Monica",
+  //     userId: 1,
+  //     profilePicture:
+  //       "https://images.pexels.com/photos/1313254/pexels-photo-1313254.jpeg?auto=compress&cs=tinysrgb&w=800",
+  //   },
+  //   {
+  //     id: 2,
+  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+  //     name: "Jenny",
+  //     userId: 2,
+  //     profilePicture:
+  //       "https://images.pexels.com/photos/16943679/pexels-photo-16943679/free-photo-of-ranti-marsyanda-chandri-anggara.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  // ];
   return (
     <div className="comments">
       <div className="write">
