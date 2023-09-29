f579b9670b158233851424ed4165339afaf170ce
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-09-29 15:49:00 +1300

    Feat: Create Delete a comment API

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -51,20 +51,50 @@ router.post("/:id", async (req, res) => {
 
 //Get comments
 router.get("/:id", async (req, res) => {
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not logged in!");
-    jwt.verify(token, "secretkey", (err) => {
-      if (err) return res.status(403).json("Token is not valid!");
-    });
-  
-    const postId = req.params.id; //Get the post id from the request params
-  
-    try {
-      const comments = await Comment.find({ postId: postId }); // Find the comments ralated to the post
-      res.status(200).json(comments);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+  jwt.verify(token, "secretkey", (err) => {
+    if (err) return res.status(403).json("Token is not valid!");
   });
+
+  const postId = req.params.id; //Get the post id from the request params
+
+  try {
+    const comments = await Comment.find({ postId: postId }); // Find the comments ralated to the post
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Delete a comment
+router.delete("/:id", async (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+  const userInfo = await verifyToken(token); // Get user information from the token
+  const currentUserId = userInfo.userId; // Get the current user's ID from the token
+  const commentId = req.params.id; // Get the comment ID from the request parameters
+
+  try {
+    //Find the comment
+    const comment = await Comment.findById(commentId);
+
+    // Check if the comment exists
+    if (!comment) {
+      return res.status(404).json("Comment not found.");
+    }
+
+    // Ensure the user is the owner of the comment
+    if (comment.userId === currentUserId) {
+      //Find and delete the comment
+      await Comment.findByIdAndDelete(commentId);
+      res.status(200).json("The comment has been deleted");
+    } else {
+      return res.status(403).json("You can only delete your own comment.");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
